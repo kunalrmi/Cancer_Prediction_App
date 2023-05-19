2f7a68f4cf06e180d3fbb55a2cc7d003764bf4a9
kunalrmi <kunalkmrrmi@gmail.com>
2023-05-19 13:27:10 +1200

    second day

--- model/main.py
+++ model/main.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
+
 def get_clean_data():
-    data = pd.read_csv('data/data.csv')
-    print(data.head())
+    data = pd.read_csv("data/data.csv")
+
+    data = data.drop(["Unnamed: 32", "id"], axis=1)
+
+    data["diagnosis"] = data["diagnosis"].map({"M": 1, "8": 0})
     return data
+
 
 def main():
     data = get_clean_data()
+    print(data.head())
 
-    if __name__ =="__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
